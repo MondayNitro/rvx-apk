8fda020ff12c049926101aa775a77797967a51d8
MondayNitro <87489540+MondayNitro@users.noreply.github.com>
2025-06-26 11:54:14 +0530

    revanced

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import requests
 
 def main():
     # Specify the desired version
-    desired_version = "20.12.46"
+    desired_version = "20.13.41"
 
     # Format the version for the URL
     formatted_version = desired_version.replace(".", "-")
