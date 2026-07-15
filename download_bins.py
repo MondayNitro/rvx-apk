a7c6fe89ecea86a596203efe6193175bcf32c6ed
MondayNitro <87489540+MondayNitro@users.noreply.github.com>
2026-07-15 21:21:06 +0530

    cli name change

--- download_bins.py
+++ download_bins.py
@@ -58,7 +58,7 @@ def download_morphe_cli(include_prereleases: bool = False):
     print("Downloading morphe cli")
     download_release_asset(
         "MorpheApp/morphe-cli",
-        r"^morphe-cli.*-all\.jar$",
+        r"^morphe-desktop.*-all\.jar$",
         "bins",
         "morphe-cli.jar",
         include_prereleases=include_prereleases,
